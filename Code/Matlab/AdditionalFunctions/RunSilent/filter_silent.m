bb9cceb12571e7af208e120a5d1207c25e6a6e47
Christoph Fruehlinger <c.fruehlinger@gmail.com>
2025-07-24 14:13:32 +0200

    removed unnecessary folder

--- Code/Matlab/AdditionalFunctions/RunSilent/filter_silent.m
+++
@@ -1,3 +0,0 @@
-function [EEG,string,coeff] = filter_silent(EEG, locutoff, hicutoff)
-	[~,EEG,string,coeff] = evalc("pop_eegfiltnew(EEG,locutoff, hicutoff,[],logical(1),[],0)");
-end
--- Code/Matlab/AdditionalFunctions/RunSilent/runica_silent.m
+++
@@ -1,4 +0,0 @@
-function EEG = runica_silent(EEG, rank)
-	[~, EEG] = evalc("pop_runica(EEG,'icatype','runica','pca',rank,'extended',1,'interupt','off')");
-end
-
